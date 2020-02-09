f9cbe2106ff76a116aff4d7c702bbf18a43448b9
Yusuf Topal <40267286+yusufTopal@users.noreply.github.com>
2020-02-09 19:13:29 +0100

    Update route_planner.h

--- src/route_planner.h
+++ src/route_planner.h
@@ -10,11 +10,9 @@
 class RoutePlanner {
   public:
     RoutePlanner(RouteModel &model, float start_x, float start_y, float end_x, float end_y);
-    // Add public variables or methods declarations here.
     float GetDistance() const {return distance;}
     void AStarSearch();
 
-    // The following methods have been made public so we can test them individually.
     void AddNeighbors(RouteModel::Node *current_node);
     float CalculateHValue(RouteModel::Node const *node);
     std::vector<RouteModel::Node> ConstructFinalPath(RouteModel::Node *);
@@ -22,7 +20,6 @@ class RoutePlanner {
     static bool Compare(const RouteModel::Node *x, const RouteModel::Node *y);
 
   private:
-    // Add private variables or methods declarations here.
     std::vector<RouteModel::Node*> open_list;
     RouteModel::Node *start_node;
     RouteModel::Node *end_node;
